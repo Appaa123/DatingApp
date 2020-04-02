fe5f105bed10e27eeb49802857899f4967e509ff
Appaarao <appaarao22@gmail.com>
2020-04-02 23:24:53 +0530

    Error Handling

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -48,8 +48,9 @@ namespace DatingApp.API.Controllers
 
        [HttpPost("login")]
 
-       public async Task<IActionResult> Login(UserForLoginDto userForLoginDto){
+       public async Task<IActionResult> Login(UserForLoginDto userForLoginDto) {
 
+          // throw new Exception("Computer says no!!!!!!!!");
            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
            if(userFromRepo == null)
@@ -84,6 +85,7 @@ namespace DatingApp.API.Controllers
             });
 
 
-       }    
+       }   
+     }
     }
-}
+ 
