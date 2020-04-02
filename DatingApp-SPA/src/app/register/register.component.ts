b055ff769a3ec637b2a9dcfab19abe63dfc6f090
Appaarao <appaarao22@gmail.com>
2020-04-03 00:22:00 +0530

    Alertify 3rd party app for alerts

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
-  constructor(private authservice: AuthService) { }
+  constructor(private authservice: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -18,10 +19,10 @@ export class RegisterComponent implements OnInit {
   register() {
 
     this.authservice.register(this.model).subscribe(() => {
-      console.log('Registerd succesfully');
+      this.alertify.success('Registerd succesfully');
     // tslint:disable-next-line: no-shadowed-variable
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
@@ -29,7 +30,7 @@ export class RegisterComponent implements OnInit {
 
     this.cancelRegister.emit(false);
 
-    console.log('cancelled Successfully');
+  //  this.alertify.success('cancelled Successfully');
 
   }
 
